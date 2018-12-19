d3e426a6c153cace9fa36c2c1e3bab98334d5116
Pablo Paglilla <pablopaglilla16@gmail.com>
2018-12-19 00:02:51 -0300

    Add reverse polish notation printer

---
+++ src/com/pablopaglilla/jlox/Printer.java
@@ -0,0 +1,71 @@
+package com.pablopaglilla.jlox;
+
+public abstract class Printer implements Expr.Visitor<String>{
+
+    static class AstPrinter extends Printer{
+
+        String transform(String name, Expr... exprs) {
+            StringBuilder builder = new StringBuilder();
+
+            builder.append("(").append(name);
+            for (Expr expr : exprs) {
+                builder.append(" ");
+                builder.append(expr.accept(this));
+            }
+            builder.append(")");
+
+            return builder.toString();
+        }
+
+       public String visitGroupingExpr(Expr.Grouping expr) {
+            return transform("group", expr.expression);
+        }
+
+    }
+
+    static class RpnAstPrinter extends Printer {
+
+        String transform(String name, Expr... exprs) {
+            StringBuilder builder = new StringBuilder();
+
+            for(Expr expr : exprs) {
+                builder.append(" ");
+                builder.append(expr.accept(this));
+            }
+            builder.append(" ").append(name);
+
+            return builder.toString();
+        }
+
+        public String visitGroupingExpr(Expr.Grouping expr) {
+            return expr.expression.accept(this);
+        }
+
+    }
+
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return transform(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
+    public abstract String visitGroupingExpr(Expr.Grouping expr);
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null) return "nil";
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return transform(expr.operator.lexeme, expr.right);
+    }
+
+    abstract String transform(String name, Expr... exprs);
+
+}

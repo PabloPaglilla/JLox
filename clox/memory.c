3226110253ebe3b128d82403d0ef54dd031a6de6
Pablo Paglilla <pablopaglilla16@gmail.com>
2019-01-05 14:24:21 -0300

    Add code from 'Strings' chapter

--- clox/memory.c
+++ clox/memory.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>                                               
 
-#include "common.h"                                               
+#include "common.h"
+#include "vm.h"
 #include "memory.h"                                               
 
 void* reallocate(void* previous, size_t oldSize, size_t newSize) {
@@ -10,4 +11,24 @@ void* reallocate(void* previous, size_t oldSize, size_t newSize) {
   }                                                               
 
   return realloc(previous, newSize);                              
+}
+
+static void freeObject(Obj* object) {                     
+  	switch (object->type) {                                 
+    	case OBJ_STRING: {                                    
+      		ObjString* string = (ObjString*)object;             
+      		FREE_ARRAY(char, string->chars, string->length + 1);
+      		FREE(ObjString, object);                            
+      		break;                                              
+    	}                                                     
+  	}                                                       
+}
+
+void freeObjects() {         
+  	Obj* object = vm.objects;  
+  	while (object != NULL) {   
+    	Obj* next = object->next;
+    	freeObject(object);      
+    	object = next;           
+  	}                          
 }
